f8b51086e16f99d8fe297c7889fcf8a44a210b75
swyam sharma <84120511+areycruzer@users.noreply.github.com>
2024-08-31 17:41:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,14 +21,14 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94702782946", "+94702782946"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94702782946", "+94702782946"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94702782946"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
-global.author = "SACHITH";                               //Do not change.
-global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
+global.Owner = ["+6285642264375", "+6285642264375"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+6285642264375", "+6285642264375"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+6285642264375"];
+global.OwnerName = "CRUZER";
+global.BotName = "CHATBOT";
+global.packname = "TICKETIT";                             //Do not change.
+global.author = "CRUZER";                               //Do not change.
+global.BotSourceCode = "https://github.com/areycruzer"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
 
@@ -45,7 +45,7 @@ global.location = "HambanThota, Sri Lankan";
 global.reactmoji = "💞";
 global.themeemoji = "😂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
-global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
+global.websitex = "https://www.linkedin.com/in/areyswyam/";
 global.lolhuman = "Sachith";
 
 
